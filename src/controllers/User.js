1021924e78b101310aa0bba33bbd05dd8585a7ca
panudet seangwiset <panudech.sean@northbkk.ac.th>
2023-08-08 13:48:16 +0700

    Add Get One User

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -2,73 +2,89 @@ const ldap = require('ldapjs');
 const { Config } = require('../config/Index');
 const { Pool } = require('pg');
 
-function oneUser(EmployeeID, callback) {
-    const client = ldap.createClient({
-        url: Config.url
-    });
+const pgPool = new Pool({
+    user: 'postgres',
+    password: 'Pass@27052002',
+    host: 'localhost',
+    database: 'blueleave',
+    port: 5432,
+});
 
-    client.bind(Config.adminDN, Config.adminPass, (err) => {
-        if (err) {
-            client.unbind();
-            return callback(err);
-        }
 
-        const searchOptions = {
-            filter: `(employeeID=${EmployeeID})`,
-            scope: 'sub',
-            attributes: ['cn', 'sn', 'company', 'sAMAccountName', 'mail', 'department'],
-        };
+async function oneUser(EmployeeID) {
+    return new Promise((resolve, reject) => {
+        const client = ldap.createClient({
+            url: Config.url
+        });
 
-        client.search(Config.baseDN, searchOptions, (searchErr, searchRes) => {
-            if (searchErr) {
-                console.error('LDAP search error:', searchErr);
-                reject(searchErr);
+        client.bind(Config.adminDN, Config.adminPass, async (err) => {
+            if (err) {
+                client.unbind();
+                reject(err);
             }
-            const entries = [];
-            searchRes.on('searchEntry', (entry) => {
-                entries.push(entry.pojo);
-            });
 
-            searchRes.on('error', (error) => {
-                client.unbind();
-                return callback(error);
-            });
+            const searchOptions = {
+                filter: `(employeeID=${EmployeeID})`,
+                scope: 'sub',
+                attributes: ['cn', 'sn', 'company', 'sAMAccountName', 'mail', 'department'],
+            };
 
-            searchRes.on('end', () => {
-                client.unbind();
-                return callback(null, entries);
-            });
+            try {
+                const entries = await new Promise((resolve, reject) => {
+                    const results = [];
+                    client.search(Config.baseDN, searchOptions, (searchErr, searchRes) => {
+                        if (searchErr) {
+                            client.unbind();
+                            reject(searchErr);
+                        }
+
+                        searchRes.on('searchEntry', (entry) => {
+                            results.push(entry.pojo);
+                        });
+
+                        searchRes.on('error', (error) => {
+                            client.unbind();
+                            reject(error);
+                        });
+
+                        searchRes.on('end', () => {
+                            client.unbind();
+                            resolve(results);
+                        });
+                    });
+                });
+
+                resolve(entries);
+            } catch (error) {
+                reject(error);
+            }
         });
     });
 }
 
-async function postgresData() {
+
+async function postgresData(EmployeeID) {
     try {
         const query = `
-        SELECT
-          employee.employee_id,
-          gender.gender_name,
-          date_of_birth.birth_date,
-          date_entered.entered_date,
-          role.role_name,
-          position.position_name
-        FROM
-          employee
-        LEFT JOIN gender ON employee.gender_id = gender.gender_id
-        LEFT JOIN date_of_birth ON employee.birth_id = date_of_birth.birth_id
-        LEFT JOIN date_entered ON employee.entered_id = date_entered.entered_id
-        LEFT JOIN role ON employee.role_id = role.role_id
-        LEFT JOIN position ON employee.position_id = position.position_id;
-      `;
+        SELECT e.*, g.gender_name, b.birth_date, d.entered_date, r.role_name, p.position_name
+        FROM employee e
+        JOIN gender g ON e.gender_id = g.gender_id
+        JOIN date_of_birth b ON e.birth_id = b.birth_id
+        JOIN date_entered d ON e.entered_id = d.entered_id
+        JOIN role r ON e.role_id = r.role_id
+        JOIN position p ON e.position_id = p.position_id
+        WHERE e.employee_id = $1`;
+        const values = [EmployeeID];
 
-        const { rows } = await pgPool.query(query);
-        return rows;
-    } catch (err) {
-        console.error('Error executing query:', err);
-        throw err;
+        const result = await pgPool.query(query, values);
+
+        return result.rows[0];
+    } catch (error) {
+        throw error;
     }
 }
 
 module.exports = {
-    oneUser
+    oneUser,
+    postgresData
 };
--- src/routes/User.js
+++ src/routes/User.js
@@ -1,38 +1,49 @@
 const express = require('express');
 const router = express.Router();
 const {
-    LdapSearchOneUser
+    oneUser,
+    postgresData
 } = require('../controllers/User')
 const {
     VerifyToken
 } = require('../midleware/Auth');
 
-router.get('/user/:EmployeeID', (req, res) => { // Express route สำหรับแสดงข้อมูลผู้ใช้คนเดียว
+router.get('/user/:EmployeeID', async (req, res) => { // Express route สำหรับแสดงข้อมูลผู้ใช้คนเดียว
     const EmployeeID = req.params.EmployeeID;
 
-    LdapSearchOneUser(EmployeeID, (err, userData) => {
-        if (err) {
-            return res.status(500).json({
-                error: 'Failed to fetch user data'
-            });
-        }
+    try {
+        const [ldapData, pgData] = await Promise.all([
+            oneUser(EmployeeID),
+            postgresData(EmployeeID),
+        ]);
 
-        if (userData.length === 0) {
-            return res.status(404).json({
-                error: 'User not found'
-            });
-        }
+        if (ldapData.length > 0) {
+            const ldapEntry = ldapData[0];
+            const ldapFormattedData = {
+                id: ldapEntry.attributes.find(attr => attr.type === "employeeID")?.values[0],
+                name: ldapEntry.attributes.find(attr => attr.type === "cn")?.values[0],
+                lastname: ldapEntry.attributes.find(attr => attr.type === "sn")?.values[0],
+                company: ldapEntry.attributes.find(attr => attr.type === "company")?.values[0],
+                email: ldapEntry.attributes.find(attr => attr.type === "mail")?.values[0],
+                status: ldapEntry.attributes.find(attr => attr.type === "userAccountControl")?.values[0] === "66048" ? "active" : "inactive",
+            };
 
-        const reformattedData = userData.map((entry) => ({
-            name: entry.attributes.find(attr => attr.type === "cn")?.values[0],
-            lastname: entry.attributes.find(attr => attr.type === "sn")?.values[0],
-            email: entry.attributes.find(attr => attr.type === "mail")?.values[0],
-            username: entry.attributes.find(attr => attr.type === "sAMAccountName")?.values[0],
-            company: entry.attributes.find(attr => attr.type === "company")?.values[0],
-            department: entry.attributes.find(attr => attr.type === "department")?.values[0]
-        }))
-        res.json(reformattedData);
-    });
+            const pgFormattedData = {
+                id: pgData.employee_id,
+                gender: pgData.gender_name,
+                birth_date: pgData.birth_date,
+                entered_date: pgData.entered_date,
+                role_name: pgData.role_name,
+                position_name: pgData.position_name,
+            };
+
+            res.json({ ldapEntry: ldapFormattedData, employeeData: pgFormattedData });
+        } else {
+            res.status(404).json({ error: 'LDAP Entry Not Found' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
 
 module.exports = router;
