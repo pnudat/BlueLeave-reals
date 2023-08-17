4bd4abdebe4298278efd63db210c6cf49ac96eba
panudet seangwiset <panudech.sean@northbkk.ac.th>
2023-08-18 06:58:04 +0700

    edit code

--- src/routes/user.router/inform.js
+++ src/routes/user.router/inform.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const moment = require('moment');
+const { getUser,
+    informData,
+    informCreate,
+    sendLineNotify,
+    formatDate } = require('../../controllers/informService');
 
 const routers = express.Router();
 
 routers.get('/inform/:id', async (req, res) => {
     const EmployeeID = req.params.id;
-    
+
     try {
         const data = await informData(EmployeeID);
 
@@ -28,28 +33,24 @@ routers.post('/inform/:id', async (req, res) => {
     const informType = req.body.informType;
     const description = req.body.description;
 
-    if (informType === 'ทำงานนอกสถานที่' || informType === 'เข้างานสาย') {
-        try {
-            const employeeData = await fetchEmployeeFromLDAP(EmployeeID);
+    try {
+        const employeeData = await getUser(EmployeeID);
 
-            if (employeeData.length > 0) {
-                const employeeInfo = employeeData[0];
-                const ldapName = { name: `${employeeInfo.attributes.find(attr => attr.type === "cn")?.values[0]} ${employeeInfo.attributes.find(attr => attr.type === "sn")?.values[0]}` };
-                const ldapNameString = ldapName.name;
-                // console.log(ldapNameString);
-                const dateData = await formatDate(informDate);
-                await informCreate(EmployeeID, dateData, informType, description)
-                await sendLineNotify(ldapNameString, informDate, informType, description);
+        if (employeeData.length > 0) {
+            const employeeInfo = employeeData[0];
+            const ldapName = { name: `${employeeInfo.attributes.find(attr => attr.type === "cn")?.values[0]} ${employeeInfo.attributes.find(attr => attr.type === "sn")?.values[0]}` };
+            const ldapNameString = ldapName.name;
+            // console.log(ldapNameString);
+            const dateData = await formatDate(informDate);
+            await informCreate(EmployeeID, dateData, informType, description)
+            await sendLineNotify(ldapNameString, informDate, informType, description);
 
-                res.status(200).json({ message: 'Line Notify sent successfully' });
-            } else {
-                res.status(404).json({ error: 'Employee not found in LDAP' });
-            }
-        } catch (error) {
-            res.status(500).json({ error: 'Failed to send Line Notify' });
+            res.status(200).json({ message: 'Line Notify sent successfully' });
+        } else {
+            res.status(404).json({ error: 'Employee not found in LDAP' });
         }
-    } else {
-        res.status(400).json({ error: 'Invalid informType value' });
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to send Line Notify' });
     }
 });
 
